b2cc14cc38e23b554a76aa24780767ed487bdf86
flobeck <beckerfloh@gmail.com>
2015-05-14 14:23:10 +0200

    add algorithm/tangentsegment

---
+++ src/algorithm/tangentsegment.py
@@ -0,0 +1,79 @@
+import math
+import numpy as np
+
+x = 0; y = 1
+
+def angle(vec1, vec2):
+    dotp  = np.dot(vec1,vec2)
+    norm_v1 = np.linalg.norm(vec1)
+    norm_v2 = np.linalg.norm(vec2)
+    return math.acos(dotp/(norm_v1*norm_v2))
+
+
+def getPrincipalAngle(C):
+    # C = [[x,y], [], [] .... ]
+    start_end_line = [C[-1][0]-C[0][0],C[-1][1]-C[0][1]]
+    xaxis = [1,0]
+    return angle(start_end_line,xaxis)
+
+
+def rotate(C, angle):
+    """
+    """
+    rotationMatrix = [[math.cos(angle), -math.sin(angle)],
+                      [math.sin(angle),  math.cos(angle)]]
+
+    newC = []
+    for vertex in C:
+        rotate = np.dot(rotationMatrix, vertex)
+        newC.append(rotate)
+
+    newC = zip(*newC)
+    return newC
+
+
+def minMaxTangent(C, i, j):
+    """
+    returns
+    0  if vivj is minimal tangent
+    1  if vivj is maximal tangent
+   -1  else
+    """
+    dx = C[j][0]-C[i][0]
+    dy = C[j][1]-C[i][1]
+    normal_vec = [-dy,dx]
+
+    dotvj_pre  = np.dot(normal_vec,C[j-1])
+    dotvj_post = np.dot(normal_vec,C[j+1])
+
+    # vj-1 and vj+1 are on the same site of vij
+    # <=> the dotproduct of those two vectors
+    # with the normal vector of vij has the same sign
+
+    if dotvj_pre * dotvj_post > 0:  # same sign
+        #vij is tangent
+        if C[j-1][1] < C[j][1]:
+            return 1
+        else:
+            return 0
+    return -1
+
+def intersect(a,b,c,d):
+    """
+    returns intersection point of line ab and cd
+    """
+    Sx = ((a[x]*b[y]-c[x]-d[x])-(a[x]-b[x])*(c[x]*d[y]-c[y]-d[x]))/ \
+    ((a[x]-b[x])*(c[y]-d[y])-(a[y]-b[y])*(c[x]-d[x]))
+    Sy = ((a[x]*b[y]-c[y]-d[y])-(a[y]-b[y])*(c[x]*d[y]-c[y]-d[x]))/ \
+    ((a[x]-b[x])*(c[y]-d[y])-(a[y]-b[y])*(c[x]-d[x]))
+
+    return [Sx,Sy]
+
+def computeTangentSplitters(C,i):
+    """
+    """
+    """""
+    for j in range(i+1,len(C)+1):
+        if minMaxTangent(C,i,j) == 1:
+            k = j-1
+    """""
--- src/map.py
+++ src/map.py
@@ -1,16 +1,37 @@
 import matplotlib.pyplot as plt
 from parser.gml import GML
 from map.draw import drawMap
+from algorithm.tangentsegment import *
+import numpy as np
 
 gml = GML()
 gml.getLines("../data/lines_out.txt")
 gml.getPoints("../data/points_out.txt")
 
+#drawMap(gml.linesX,gml.linesY,
+#        gml.pointsX,gml.pointsY,
+#        gml.index,
+#        annotate=True)
+"""""
+rot = []
+for i in range(0,len(gml.linesX)):
+    xy = zip(gml.linesX[i][1:], gml.linesY[i][1:])
+
+    a = t.getPrincipalAngle(xy)
+
+    #    print i, a, t.rotate(xy,a)
+    r = t.rotate(xy,a)
+    print r
+    print "              "
+    plt.plot(r[0],r[1])
+
+"""""
+
+polygonalChain = [[0,0],[10,20],[30,30],[45,22],[50,-5],[60,-10],[70,10],[75,-2],[90,15],[92,25]]  # -> paper
+C = zip(*polygonalChain)
+
+print minMaxTangent(polygonalChain,0,7)
 
 
-drawMap(gml.linesX,gml.linesY,
-        gml.pointsX,gml.pointsY,
-        gml.index,
-        annotate=True)
-
-
+plt.plot(C[0], C[1])
+plt.show()
