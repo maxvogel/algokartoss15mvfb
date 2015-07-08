55f2b456276cb63911f5abd92681392845af3fe7
flobeck <beckerfloh@gmail.com>
2015-07-08 17:51:26 +0200

    add feature; plot polygonal chain only

--- src/plot.py
+++ src/plot.py
@@ -6,35 +6,47 @@ import sys
 
 if __name__ == "__main__":
 
-    if not (len(sys.argv) == 3 or len(sys.argv) == 4):
+    if not (len(sys.argv) >= 2 and len(sys.argv) <= 4):
         print("""
         Usage:
-
+        python plot.py LineInputFilePath (without points)
+        or:
         python plot.py LineInputFilePath PointInputFilePath
         or:
-        python plot.py LineInputFilePath PointInputFilePath SimplifiedLineInputFilePath
+        python plot.py LineInputFilePath SimplifiedLineInputFilePath PointInputFilePath
 
         """)
 
         exit()
 
-    gml = GML()
-    lines = sys.argv[1]
-    points = sys.argv[2]
-    gml.readLines(lines)
-    gml.readPoints(points)
-
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 2:
+        gml = GML()
+        lines = sys.argv[1]
+        gml.readLines(lines)
         drawMap(gml.linesX, gml.linesY,
                 gml.pointsX,gml.pointsY,
                 gml.index,
                 annotate=False)
         plt.show()
 
-    elif len(sys.argv) == 4:
-        gmlSimplified = GML()
-        lines_simp = sys.argv[3]
-        gmlSimplified.readLines(lines_simp)
-        gmlSimplified.readPoints(points)
+    if len(sys.argv) > 2:
+        gml = GML()
+        lines = sys.argv[1]
+        points = sys.argv[-1]
+        gml.readLines(lines)
+        gml.readPoints(points)
 
-        plotSimplifiedAndOriginal(gml, gmlSimplified)
+        if len(sys.argv) == 3:
+            drawMap(gml.linesX, gml.linesY,
+                    gml.pointsX,gml.pointsY,
+                    gml.index,
+            annotate=False)
+            plt.show()
+
+        elif len(sys.argv) == 4:
+            gmlSimplified = GML()
+            lines_simp = sys.argv[2]
+            gmlSimplified.readLines(lines_simp)
+            gmlSimplified.readPoints(points)
+
+            plotSimplifiedAndOriginal(gml, gmlSimplified)
